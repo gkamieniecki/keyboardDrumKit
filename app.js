c7fc5f287bd4814b245dd40abfb2d33cbf656c23
gkamieniecki <grz.kamieniecki@gmail.com>
2021-04-21 12:53:25 +0100

    changed key-press events and design

--- app.js
+++ app.js
@@ -1,39 +1,33 @@
-document.addEventListener("keydown", (e) => {
-    if (e.keyCode === 65) {
-        document.getElementById("boom").play();
-    }
-    else if (e.keyCode === 83) {
-        document.getElementById("clap").play();
-    }
-    else if (e.keyCode === 68) {
-        document.getElementById("hihat").play();
-    }
-    else if (e.keyCode === 70) {
-        document.getElementById("kick").play();
-    }
-    else if (e.keyCode === 71) {
-        document.getElementById("openhat").play();
-    }
-    else if (e.keyCode === 72) {
-        document.getElementById("ride").play();
-    }
-    else if (e.keyCode === 74) {
-        document.getElementById("snare").play();
-    }
-    else if (e.keyCode === 75) {
-        document.getElementById("tink").play();
-    }
-    else if (e.keyCode === 76) {
-        document.getElementById("tom").play();
-    }
-});
+// ======================
+// ===Key-press events===
+// ======================
 
 
+const playSound = (e) => {
+    
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    audio.play();
+    
+    const key = document.querySelector(`.sound[data-key="${e.keyCode}"]`);
+    key.classList.add("clickChange");
+
+    const removeChange = (e) => {
+        
+        const key = document.querySelector(`.sound[data-key="${e.keyCode}"]`);
+        key.classList.remove("clickChange");
+    }
+    
+    document.addEventListener("keyup", removeChange);
+
+}
+
+document.addEventListener("keydown", playSound);
 
 
-// ====================
-// === Click Events ===
-// ====================
+// ==================
+// ===Click events===
+// ==================
+
 
 function playSoundA() {
     document.getElementById("boom").play();
